ba6202bf02a5b8bbb953c2fe316e2db3ee967e8f
ReChast <rdp7nsjdbk@privaterelay.appleid.com>
2026-02-06 11:00:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-function mainMenu() {
-  document.getElementById("app").innerHTML = `
-    <h1>BBX Tracker</h1>
-    <div class="menu">
-      <button onclick="openPezzi()">Pezzi</button>
-      <button onclick="openCombo()">Combo</button>
-      <button onclick="openRegistro()">Registro Risultati</button>
-    </div>
-  `;
-}
 
-mainMenu();
