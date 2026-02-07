625ba2413a318597c7091585f5c9c01bcda343a7
ReChast <rdp7nsjdbk@privaterelay.appleid.com>
2026-02-07 18:23:33 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,12 +6,15 @@ function mainMenu() {
       <button onclick="openRegistro()">Registro Risultati</button>
     </div>
 
-    <div class="external-links">
-      <a href="https://t.me/+kGy7DQaqUVRlZjI0" target="_blank" class="logo-btn">
-        <img src="assets/tglink.png" alt="Canale Telegram">
+    <div class="icon-bar">
+      <a href="https://www.instagram.com/tps_bbx?igsh=MTJuYmhjYmZpZDJxaQ%3D%3D&utm_source=qr" target="_blank" class="icon-link">
+        <img src="assets/iglink.png" alt="Link 1">
       </a>
-      <a href="https://www.instagram.com/tps_bbx?igsh=MTJuYmhjYmZpZDJxaQ%3D%3D&utm_source=qr" target="_blank" class="logo-btn">
-        <img src="assets/tglink.png" alt="Pagina Instagram">
+      <a href="https://t.me/+kGy7DQaqUVRlZjI0" target="_blank" class="icon-link">
+        <img src="assets/tglink.png" alt="Link 2">
+      </a>
+      <a href="https://link3.com" target="_blank" class="icon-link">
+        <img src="assets/logo3.png" alt="Link 3">
       </a>
     </div>
   `;
