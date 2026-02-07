b7ae2b10be6922565a7d717b41e700e3448a0c94
ReChast <rdp7nsjdbk@privaterelay.appleid.com>
2026-02-07 18:58:02 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,13 +8,13 @@ function mainMenu() {
 
     <div class="icon-bar">
       <a href="https://www.instagram.com/tps_bbx?igsh=MTJuYmhjYmZpZDJxaQ%3D%3D&utm_source=qr" target="_blank" class="icon-link">
-        <img src="assets/iglink.png" alt="Link 1">
+        <img src="assets/iglink.jpeg" alt="Link 1">
       </a>
       <a href="https://t.me/+kGy7DQaqUVRlZjI0" target="_blank" class="icon-link">
         <img src="assets/tglink.png" alt="Link 2">
       </a>
       <a href="https://link3.com" target="_blank" class="icon-link">
-        <img src="assets/logo3.png" alt="Link 3">
+        <img src="assets/logo3.jpeg" alt="Link 3">
       </a>
     </div>
   `;
