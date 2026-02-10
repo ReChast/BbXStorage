2debf400e967d488b2068b0c81ff46d0e1d57bc5
ReChast <rdp7nsjdbk@privaterelay.appleid.com>
2026-02-11 00:20:36 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,16 +6,32 @@ function mainmenu() {
       <button onclick="opencombo()">Combo</button>
       <button onclick="openRegistroEventi()">Registro Eventi</button>
       
-      <hr style="width:100%; border:0; border-top:1px solid #333; margin:10px 0;">
+      <hr style="width:100%; border:0; border-top:1px solid #333; margin:15px 0;">
       
       <button onclick="downloadBackup()" class="secondary">💾 Scarica Backup</button>
       <button onclick="uploadBackup()" class="secondary">📂 Carica Backup</button>
     </div>
-    
+
     <div class="icon-bar">
+      
+      <div class="social-item">
+        <a href="https://www.instagram.com/tps_bbx" target="_blank" class="icon-link">
+          <img src="assets/iglink.jpeg" alt="IG">
+        </a>
+        <span>Instagram</span>
       </div>
+
+      <div class="social-item">
+        <a href="https://t.me/+kGy7DQaqUVRlZjI0" target="_blank" class="icon-link">
+          <img src="assets/tglink.jpeg" alt="TG">
+        </a>
+        <span>Telegram</span>
+      </div>
+
+    </div>
   `;
 }
+
 
 /* ====== SISTEMA DI BACKUP ====== */
 
